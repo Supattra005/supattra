485525d6a4d96fba194150fc9ae70f3943b7c16d
Supattra005 <supattrathongprom@gmail.com>
2025-05-05 00:55:00 +0700

    week....

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/test', function () {
+    return Inertia ::render('Test');
+})->name('test');
+
+
+
+Route::get('/tictactoe', function () {
+    return Inertia::render('Tictactoe');
+})->name('tictactoe');
+
+
+
+Route::get('/fruit', function () {
+    return Inertia::render('Fruit');
+})->name('fruit');
+
